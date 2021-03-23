b7c1a4c43f0b8c4a16a3307dd933d0c3d2c4f4a3
Nikolai <nist2902@yandex.ru>
2021-03-23 14:12:00 +0300

    работа в процессе

---
+++ api/models.py
@@ -0,0 +1,69 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name="reviews",
+        null=False
+    )
+    text = models.TextField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="reviews", 
+        null=False
+    )
+    score = models.IntegerField(
+        "score",
+        null=False,
+        validators=[MinValueValidator(1, "Необходимо > 1"),
+                    MaxValueValidator(10, "Необходимо < 10")]
+    )
+    pub_date = models.DateTimeField(
+        "review pub date",
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ("-pub_date",)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        null=False
+    )
+    text = models.TextField(
+        "text of comment",
+        null=False
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        null=False
+    )
+    pub_date = models.DateTimeField(
+        "comment pub date",
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ("-pub_date",)
+
+
+class Genre(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass

68c2af257053f1fa1466eb191e8a0160d80b72f4
Vladimir <vrmy87@yandex.ru>
2021-03-25 23:54:16 +0700

    fixed some errors 5

--- api/models.py
+++ api/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+#from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
+#User = get_user_model()
 
 ROLES_CHOICES = [
     ('user', 'Пользователь'),

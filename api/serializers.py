2c5a1b8737d72ac1e812d97231ff43c8c6757aff
Vladimir <vrmy87@yandex.ru>
2021-03-27 02:09:38 +0700

    deleted the someone funtion

--- api/serializers.py
+++ api/serializers.py
@@ -105,18 +105,3 @@ class ConfirmationCodeSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('email',)
         model = YamdbUser
-
-
-class GetTokenSerializer(serializers.ModelSerializer):
-    confirmation_code = serializers.StringRelatedField(many=False)
-
-    def validate_email(self, value):
-        if not YamdbUser.objects.filter(email=value).exists():
-            return serializers.ValidationError('You cant follow yourself')
-        return value
-
-    class Meta:
-        fields = ('email', 'confirmation_code',)
-        read_only_fields = ['email']
-        extra_kwargs = {'confirmation_code': {'required': True}}
-        model = YamdbUser

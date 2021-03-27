af2f4213376209afbb406c12d1bd097530064059
Vladimir <vrmy87@yandex.ru>
2021-03-27 20:41:44 +0700

    deleted other code

--- api/serializers.py
+++ api/serializers.py
@@ -32,22 +32,12 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class GenreField(serializers.SlugRelatedField):
-    def to_internal_value(self, data):
-        try:
-            return self.get_queryset().get(**{self.slug_field: data})
-        except (TypeError, ValueError):
-            self.fail('invalid')
 
     def to_representation(self, value):
         return GenreSerializer(value).data
 
 
 class CategoryField(serializers.SlugRelatedField):
-    def to_internal_value(self, data):
-        try:
-            return self.get_queryset().get(**{self.slug_field: data})
-        except (TypeError, ValueError):
-            self.fail('invalid')
 
     def to_representation(self, value):
         return CategorySerializer(value).data

cf3d179770fdd0f4aef3ce1f0332085b8dae7f3b
Vladimir <vrmy87@yandex.ru>
2021-03-28 20:27:25 +0700

    fixed some errors

--- api/views.py
+++ api/views.py
@@ -108,10 +108,11 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsReadOnly | IsAdmin | IsModerator | IsAuthor,)
-
+    
     def perform_create(self, serializer):
         """Сохранение комментария в бд. """
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, review=review)
 
     def get_queryset(self):
